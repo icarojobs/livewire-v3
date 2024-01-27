1e322b4a4c99343b26519d5f1700181c34b93415
Tio Jobs <icarojobsoficial@gmail.com>
2024-01-27 09:56:46 -0300

    typo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 
-Route::get('/', Counter::class);
+
+Route::get('/', [TaskController::class, 'index']);
+
+//Route::get('/', Counter::class);
